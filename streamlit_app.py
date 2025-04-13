c45c48aec565e216bcad53dabecff0c4c217895e
Varun Chandar <102123084@nitt.edu>
2025-04-13 21:55:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import os
 import json
 import pandas as pd
+pip install transformers
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import numpy as np
 import subprocess
